7562eb4deb4145f386fdbeb20baf16eb55671b01
Theowl <46950858+ADsimple@users.noreply.github.com>
2025-09-26 13:13:13 +0700

    RevoU Test

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const name = prompt("Masukan Nama Kamu:");
+
+if (name) {
+  document.getElementById("username").textContent = name;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contactForm");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // cegah reload halaman
+
+    // Ambil value dari input
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    // Validasi manual
+    if (!name || !email || !message) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Semua field wajib diisi!",
+      });
+      return;
+    }
+
+    // Kalau sukses
+    Swal.fire({
+      icon: "success",
+      title: `Thanks, ${name}!`,
+      text: "Form submitted successfully!",
+      confirmButtonColor: "#C8A00C",
+    });
+
+    // Reset form setelah submit
+    form.reset();
+  });
+});
---
+++ js/tailwind-config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        helvetica: ['"Helvetica Neue"', "Helvetica", "Arial", "sans-serif"],
+        redhat: ['"Red Hat Display"', "sans-serif"],
+      },
+    },
+  },
+};
